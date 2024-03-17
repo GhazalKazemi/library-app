46676dc7b8c0b49b091fa22fbf5de34bbf60edd8
Ghazal <37179631+GhazalKazemi@users.noreply.github.com>
2024-03-18 10:41:33 +1100

    Cleanup static frontend

--- frontend/react-library/src/App.tsx
+++ frontend/react-library/src/App.tsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/navbar-footer/Navbar';
-import { ExploreTopBooks } from './layouts/homepage/ExploreTopBooks';
-import { Carousel } from './layouts/homepage/Carousel';
-import { Heros } from './layouts/homepage/Heros';
-import { LibraryServices } from './layouts/homepage/LibraryServices';
 import { Footer } from './layouts/navbar-footer/Footer';
+import { HomePage } from './layouts/homepage/HomePage';
 
-function App() {
+export const App = () => {
   return (
     <div>
       <Navbar />
-      <ExploreTopBooks />
-      <Carousel />
-      <Heros/>
-      <LibraryServices/>
-      <Footer/>
+      <HomePage />
+      <Footer />
     </div>
   );
 }
