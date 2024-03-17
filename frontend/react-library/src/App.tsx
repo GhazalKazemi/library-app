eec26d785f6247195f7ac20fbcbdf3248a02c440
Ghazal <37179631+GhazalKazemi@users.noreply.github.com>
2024-03-17 13:10:11 +1100

    Add static Carousel component

---
+++ frontend/react-library/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import { Navbar } from './layouts/navbar-footer/Navbar';
+import { ExploreTopBooks } from './layouts/homepage/ExploreTopBooks';
+import { Carousel } from './layouts/homepage/Carousel';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <ExploreTopBooks />
+      <Carousel />
+    </div>
+  );
+}
+
+export default App;
