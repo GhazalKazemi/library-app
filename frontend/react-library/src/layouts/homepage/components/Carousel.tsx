69c1026e489eda8a331db2caa58c3556fffdbe39
Ghazal <37179631+GhazalKazemi@users.noreply.github.com>
2024-03-18 14:30:09 +1100

    Fetch books for Carousel component

--- frontend/react-library/src/layouts/homepage/components/Carousel.tsx
+++ frontend/react-library/src/layouts/homepage/components/Carousel.tsx
@@ -1,13 +1,49 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ReturnBook } from "./ReturnBook";
 import BookModel from "../../../models/BookModel";
+import { error } from "console";
 
-// import Images from './../../Images';
 export const Carousel = () =>{
     const [books, setBooks] = useState<BookModel[]>([]);
-    const [isLoading, setIsLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
     const [httpError, setHttpError] = useState(null);
-    
+
+    useEffect(() => {
+        const fetchBooks = async () => {
+            const baseUrl: string = "http://localhost:8080/api/books";
+            const url: string = `${baseUrl}?page=0&size=9` //fetch 9 books for the carousel
+            const response = await fetch(url);
+            if(!response.ok){
+                throw new Error("Something went wrong!");
+            }
+            const responseJSON = await response.json();
+            const responseData = responseJSON._embedded.books;
+
+            const loadedBooks: BookModel[] = [];
+
+            for(const key in responseData){
+                loadedBooks.push({
+                    id: responseData[key].id,
+                    title: responseData[key].title,
+                    author: responseData[key].author,
+                    description: responseData[key].description,
+                    copies: responseData[key].copies,
+                    copiesAvailable: responseData[key].copiesAvailable,
+                    category: responseData[key].category,
+                    img: responseData[key].img
+                });
+            }
+
+            setBooks(loadedBooks);
+            setIsLoading(false);
+
+        };
+        fetchBooks().catch((error: any) => {
+            setIsLoading(false);
+            setHttpError(error.message);
+        })
+    }, []);
+
     return(
         <div className='container mt-5' style={{ height: 550 }}>
             <div className='homepage-carousel-title'>
