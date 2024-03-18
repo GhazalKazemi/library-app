c34bd38b615170ea4068f46918030d428a44e0b8
Ghazal <37179631+GhazalKazemi@users.noreply.github.com>
2024-03-18 12:13:47 +1100

    Configure Rest Data to Allow Get method only

---
+++ backend/spring-boot-library/src/main/java/com/ghazal/springbootlibrary/config/LibraryAppDataRestConfig.java
@@ -0,0 +1,42 @@
+package com.ghazal.springbootlibrary.config;
+
+import com.ghazal.springbootlibrary.entity.Book;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class LibraryAppDataRestConfig implements RepositoryRestConfigurer {
+    private String theAllowedOrigins = "http://localhost:3000";
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config,
+                                                     CorsRegistry cors) {
+        HttpMethod[] theUnsupportedActions = {
+                HttpMethod.POST,
+                HttpMethod.PATCH,
+                HttpMethod.DELETE,
+                HttpMethod.PUT};
+
+        config.exposeIdsFor(Book.class);
+
+        disableHttpMethods(Book.class, config, theUnsupportedActions);
+
+        /* Configure CORS Mapping */
+        cors.addMapping(config.getBasePath() + "/**")
+                .allowedOrigins(theAllowedOrigins);
+    }
+    private void disableHttpMethods(Class theClass,
+                                    RepositoryRestConfiguration config,
+                                    HttpMethod[] theUnsupportedActions) {
+        config.getExposureConfiguration()
+                .forDomainType(theClass)
+                .withItemExposure((metadata, httpMethods) ->
+                        httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metadata, httpMethods) ->
+                        httpMethods.disable(theUnsupportedActions));
+    }
+
+}
