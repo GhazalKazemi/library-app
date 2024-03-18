5c5fe0b1cf5eed0ca43be5b10646bfefe47bc936
Ghazal <37179631+GhazalKazemi@users.noreply.github.com>
2024-03-18 11:34:00 +1100

    Create Book Entity

---
+++ backend/spring-boot-library/src/main/java/com/ghazal/springbootlibrary/entity/Book.java
@@ -0,0 +1,31 @@
+package com.ghazal.springbootlibrary.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "book")
+@Data
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "copies")
+    private String copies;
+
+    @Column(name = "copies_available")
+    private String copiesAvailable;
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "category")
+    private String category;
+    @Column(name = "image")
+    private String image;
+}
