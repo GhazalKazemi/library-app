0153d09870541afac6fca330ba37e8c4c8f36132
Ghazal <37179631+GhazalKazemi@users.noreply.github.com>
2024-03-19 14:22:26 +1100

    Add API for searching book by title

--- backend/spring-boot-library/src/main/java/com/ghazal/springbootlibrary/dao/BookRepository.java
+++ backend/spring-boot-library/src/main/java/com/ghazal/springbootlibrary/dao/BookRepository.java
@@ -1,9 +1,13 @@
 package com.ghazal.springbootlibrary.dao;
 
 import com.ghazal.springbootlibrary.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Integer> {
+    Page findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
 }
