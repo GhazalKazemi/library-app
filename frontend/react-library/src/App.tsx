01fdca655613c756ebf57b188d68704a96035e5e
Ghazal <37179631+GhazalKazemi@users.noreply.github.com>
2024-03-20 11:21:21 +1100

    Make footer stick to the bottom of the page

--- frontend/react-library/src/App.tsx
+++ frontend/react-library/src/App.tsx
@@ -8,20 +8,22 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 
 export const App = () => {
   return (
-    <div>
+    <div className='d-flex flex-column min-vh-100'>
       <Navbar />
-      {/* React Router Dom v6 -- for v5 need to use Switch instead and element is not needed */}
-      <Routes> 
-      <Route path='/' element={<Navigate to='/home'/>}>
-          
-        </Route>
-        <Route path='/home' element={<HomePage />}>
-          {/* <HomePage /> */}
-        </Route>
-        <Route path='/search' element={<SearchBooksPage/>}>
-          {/* <SearchBooksPage /> */}
-        </Route>
-      </Routes>
+      <div className='flex-grow-1'>
+        {/* React Router Dom v6 -- for v5 need to use Switch instead and element is not needed */}
+        <Routes>
+          <Route path='/' element={<Navigate to='/home' />}>
+
+          </Route>
+          <Route path='/home' element={<HomePage />}>
+            {/* <HomePage /> */}
+          </Route>
+          <Route path='/search' element={<SearchBooksPage />}>
+            {/* <SearchBooksPage /> */}
+          </Route>
+        </Routes>
+      </div>
       <Footer />
     </div>
   );
