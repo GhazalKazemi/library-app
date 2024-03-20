3e5c3d437a432b34d4eb9abd027793579e32d24f
Ghazal <37179631+GhazalKazemi@users.noreply.github.com>
2024-03-20 11:04:21 +1100

    Implement react routing v6

--- frontend/react-library/src/App.tsx
+++ frontend/react-library/src/App.tsx
@@ -4,13 +4,24 @@ import { Navbar } from './layouts/navbar-footer/Navbar';
 import { Footer } from './layouts/navbar-footer/Footer';
 import { HomePage } from './layouts/homepage/HomePage';
 import { SearchBooksPage } from './layouts/SearchBooks/SearchBooksPage';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 export const App = () => {
   return (
     <div>
       <Navbar />
-      {/* <HomePage /> */}
-      <SearchBooksPage />
+      {/* React Router Dom v6 -- for v5 need to use Switch instead and element is not needed */}
+      <Routes> 
+      <Route path='/' element={<Navigate to='/home'/>}>
+          
+        </Route>
+        <Route path='/home' element={<HomePage />}>
+          {/* <HomePage /> */}
+        </Route>
+        <Route path='/search' element={<SearchBooksPage/>}>
+          {/* <SearchBooksPage /> */}
+        </Route>
+      </Routes>
       <Footer />
     </div>
   );
