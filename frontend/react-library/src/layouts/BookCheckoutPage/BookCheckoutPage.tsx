4299987143dc93604787f23524e594fdd6bc1427
Ghazal <37179631+GhazalKazemi@users.noreply.github.com>
2024-03-20 12:03:32 +1100

    Set up useEffect to fetch a book

--- frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -1,7 +1,39 @@
+import { useEffect, useState } from "react"
+import BookModel from "../../models/BookModel"
+import { SpinnerLoading } from "../Utils/SpinnerLoading";
+
 export const BookCheckoutPage = () => {
-    return(
+    const [book, setBook] = useState<BookModel>();
+    const [isLoading, setIsLoading] = useState(true);
+    const [httpError, setHttpError] = useState(false);
+    // Take out the id from the path localhost:3000/checkout/5
+    const bookId = (window.location.pathname).split('/')[2];
+    useEffect(() => {
+        const fetchBook = async () => {
+
+        }
+        fetchBook().catch((error) => {
+            setIsLoading(false);
+            setHttpError(error.message);
+        });
+    }, []);
+
+    if(isLoading){
+        return(
+            <SpinnerLoading/>
+        )
+    }
+    if(httpError){
+        return(
+            <div className="container m-5">
+                <p>Something went wrong while fetching the book Id: {bookId} === {httpError}</p>
+            </div>
+        )
+    }
+
+    return (
         <div>
-            
+
         </div>
     )
 }
