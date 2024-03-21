7c3046380547099de6a95df7bf37a5160b21d823
Ghazal <37179631+GhazalKazemi@users.noreply.github.com>
2024-03-21 12:18:31 +1100

    Add Review Model

---
+++ backend/spring-boot-library/src/main/java/com/ghazal/springbootlibrary/entity/Review.java
@@ -0,0 +1,35 @@
+package com.ghazal.springbootlibrary.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+@Table(name = "review")
+@Data
+public class Review {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_email")
+    private  String userEmail;
+
+    @Column(name = "date")
+    @CreationTimestamp
+    private Date date;
+
+    @Column(name = "rating")
+    private  String rating;
+
+    @Column(name = "book_id")
+    private Long bookId;
+
+    @Column(name = "review_description")
+    private String reviewDescription;
+
+}
