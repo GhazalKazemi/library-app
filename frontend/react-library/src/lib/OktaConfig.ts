e2dcf028f537b860346245f3a2e748f49c140ce1
Ghazal <37179631+GhazalKazemi@users.noreply.github.com>
2024-03-22 15:58:38 +1100

    Fix Okta Config issue

--- frontend/react-library/src/lib/OktaConfig.ts
+++ frontend/react-library/src/lib/OktaConfig.ts
@@ -1,8 +1,8 @@
 import React from "react";
 
 export const OktaConfig = {
-    clientId: '0oafyjklv1uEtboOH5d7',
-    issuer: "https://dev-73615806.okta.com/oauth2/default",
+    clientId: process.env.LIBRARY_APP_OKTA_CLIENT_ID,
+    issuer: `https://${process.env.LIBRARY_APP_OKTA_BASE_URL}/oauth2/default`,
     redirectUri: "http://localhost:3000/login/callback",
     scopes: ['openid', 'profile', 'email'],
     pkce: true, 
