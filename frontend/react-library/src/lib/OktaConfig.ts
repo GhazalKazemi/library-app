2c8a3a8f7558442bfd13ac87d75298543d1c005b
Ghazal <37179631+GhazalKazemi@users.noreply.github.com>
2024-03-22 11:25:42 +1100

    Implement Okta signin widget

--- frontend/react-library/src/lib/OktaConfig.ts
+++ frontend/react-library/src/lib/OktaConfig.ts
@@ -1,9 +1,9 @@
 import React from "react";
 
 export const OktaConfig = {
-    clientId: 'nope!',
-    issuer: 'https://dev-73615806.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:3000/login/callback',
+    clientId: process.env.LIBRARY_APP_OKTA_CLIENT_ID,
+    issuer: `https://${process.env.LIBRARY_APP_OKTA_BASE_URL}/oauth2/default`,
+    redirectUri: `http://localhost:3000/login/callback`,
     scopes: ['openid', 'profile', 'email'],
     pkce: true, 
     disableHttpsCheck: true
