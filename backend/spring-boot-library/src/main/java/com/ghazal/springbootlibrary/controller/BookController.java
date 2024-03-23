c662ad846905c3ac5dc8cc812d830f584ce8637d
Ghazal <37179631+GhazalKazemi@users.noreply.github.com>
2024-03-23 14:10:13 +1100

    Create Controller for book checkout

---
+++ backend/spring-boot-library/src/main/java/com/ghazal/springbootlibrary/controller/BookController.java
@@ -0,0 +1,26 @@
+package com.ghazal.springbootlibrary.controller;
+
+import com.ghazal.springbootlibrary.entity.Book;
+import com.ghazal.springbootlibrary.service.BookService;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+    private BookService bookService;
+
+    public BookController(BookService bookService){
+        this.bookService = bookService;
+    }
+
+    @PutMapping("/secure/checkout") //will be secure in the future!
+    public Book checkoutBook(@RequestParam Long bookId) throws Exception {
+        String fakeUserEmail = "fakeUser@email.com";
+
+        return bookService.checkoutBook(fakeUserEmail, bookId);
+    }
+
+
+}
