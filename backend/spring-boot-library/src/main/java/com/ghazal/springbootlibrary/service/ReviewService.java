35d6fcb298f42364231acd151518700d7bf01c9a
Ghazal <37179631+GhazalKazemi@users.noreply.github.com>
2024-03-26 10:05:54 +1100

    Implement Review request model and service

---
+++ backend/spring-boot-library/src/main/java/com/ghazal/springbootlibrary/service/ReviewService.java
@@ -0,0 +1,44 @@
+package com.ghazal.springbootlibrary.service;
+
+import com.ghazal.springbootlibrary.dao.BookRepository;
+import com.ghazal.springbootlibrary.dao.ReviewRepository;
+import com.ghazal.springbootlibrary.entity.Review;
+import com.ghazal.springbootlibrary.requestmodels.ReviewRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Service
+@Transactional
+public class ReviewService {
+
+    private BookRepository bookRepository;
+    private ReviewRepository reviewRepository;
+
+    public ReviewService(BookRepository bookRepository, ReviewRepository reviewRepository) {
+        this.bookRepository = bookRepository;
+        this.reviewRepository = reviewRepository;
+    }
+
+    public void postReview(String userEmail, ReviewRequest reviewRequest) throws Exception {
+        Review validateReview = reviewRepository.findByUserEmailAndBookId(userEmail, reviewRequest.getBookId());
+        if(validateReview != null){
+            throw new Exception("Review already exists");
+        }
+
+        Review review = new Review();
+        review.setRating(reviewRequest.getRating());
+        review.setBookId(reviewRequest.getBookId());
+        review.setUserEmail(userEmail);
+        if(reviewRequest.getReviewDescription().isPresent()){
+            review.setReviewDescription(reviewRequest.getReviewDescription().map(
+                    Objects::toString
+            ).orElse(null));
+        }
+        review.setDate(Date.valueOf(LocalDate.now()));
+        reviewRepository.save(review);
+    }
+}
